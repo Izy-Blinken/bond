d5b04617cc31a978d2557bb060d2acf4126f8e5c
Nwleomxm-zz <2024600076@ms.bulsu.edu.ph>
2026-03-03 23:06:47 +0800

    Update ObjVehicle.java

--- src/models/ObjVehicle.java
+++ src/models/ObjVehicle.java
@@ -7,21 +7,23 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ObjVehicle extends GameObject {
-    
-    public ObjVehicle(panel gp, int type){
+
+    private static BufferedImage sheet;
+
+    public ObjVehicle(panel gp, int type) {
         try {
-            BufferedImage sheet = ImageIO.read(getClass().getResourceAsStream("/assets/no_sanctuary_map/MAP TILES.png"));
-            
-            if(type == 1){
-                image = sheet.getSubimage(284, 6, 119, 52); //bus
+            if (sheet == null)
+                sheet = ImageIO.read(getClass().getResourceAsStream("/assets/no_sanctuary_map/MAP TILES.png"));
+
+            if (type == 1) {
+                image     = sheet.getSubimage(284, 6, 119, 52); //bus
                 solidArea = new Rectangle(5, 5, 109, 42);
-            }
-            else if(type == 2){ 
-                image = sheet.getSubimage(741, 213, 96, 72); //car
+            } else if (type == 2) {
+                image     = sheet.getSubimage(741, 213, 96, 72); //car
                 solidArea = new Rectangle(5, 10, 86, 52);
             }
-            
             collision = true;
+            generateNightImage();
         } catch (IOException e) {
             e.printStackTrace();
         }
