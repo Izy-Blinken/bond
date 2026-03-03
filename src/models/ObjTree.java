deb7f35c666331d5f110aa1899589680c86aeb18
Nwleomxm-zz <2024600076@ms.bulsu.edu.ph>
2026-03-03 23:06:18 +0800

    Update ObjTree.java

--- src/models/ObjTree.java
+++ src/models/ObjTree.java
@@ -7,20 +7,23 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ObjTree extends GameObject {
- 
+
+    private static BufferedImage sheet;
+
     public ObjTree(panel gp, int type) {
         try {
-            BufferedImage sheet = ImageIO.read(getClass().getResourceAsStream("/assets/no_sanctuary_map/MAP TILES.png"));
-            
-            if(type == 1){
-                image = sheet.getSubimage(192, 68, 139, 118); //full tree
-                solidArea = new Rectangle(10, 0, 119, 118); // bottom trunk area
-            }
-            else if(type == 2){
-                image = sheet.getSubimage(741, 213, 96, 72); //tree top
+            if (sheet == null)
+                sheet = ImageIO.read(getClass().getResourceAsStream("/assets/no_sanctuary_map/MAP TILES.png"));
+
+            if (type == 1) {
+                image     = sheet.getSubimage(192, 68, 139, 118); //full tree
+                solidArea = new Rectangle(10, 0, 119, 118);
+            } else if (type == 2) {
+                image     = sheet.getSubimage(741, 213, 96, 72); //tree top
                 solidArea = new Rectangle(5, 0, 86, 72);
             }
             collision = true;
+            generateNightImage(); 
         } catch (IOException e) {
             e.printStackTrace();
         }
