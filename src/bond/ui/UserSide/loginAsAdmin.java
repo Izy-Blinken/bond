3ecd79255855497a62c155e4c1a03697896ad9ed
softnise <deniserain2@gmail.com>
2026-04-04 16:04:37 +0800

    updated user

--- src/bond/ui/UserSide/loginAsAdmin.java
+++ src/bond/ui/UserSide/loginAsAdmin.java
@@ -8,15 +8,15 @@ package bond.ui.UserSide;
  *
  * @author denis
  */
-public class loginAsOso extends javax.swing.JFrame {
+public class loginAsAdmin extends javax.swing.JFrame {
 
     /**
-     * Creates new form loginAsOso
+     * Creates new form loginAsAdmin
      */
-    public loginAsOso() {
+    public loginAsAdmin() {
         initComponents();
         
-     
+      
 
         backBtn.setForeground(java.awt.Color.WHITE);
         backBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -77,6 +77,7 @@ public class loginAsOso extends javax.swing.JFrame {
                 }
             }
         });
+        
     }
 
     /**
@@ -89,14 +90,15 @@ public class loginAsOso extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel5 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
         usernameInput = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         passwordInput = new javax.swing.JPasswordField();
-        jLabel2 = new javax.swing.JLabel();
-        login = new javax.swing.JButton();
         backBtn = new javax.swing.JButton();
+        login = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         navbar = new javax.swing.JLabel();
         dark = new javax.swing.JLabel();
         bsu = new javax.swing.JLabel();
@@ -105,15 +107,15 @@ public class loginAsOso extends javax.swing.JFrame {
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel5.setFont(new java.awt.Font("Playfair Display", 1, 24)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel5.setText("Login As OSO Admin");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 150, -1, -1));
-
         jLabel3.setFont(new java.awt.Font("Plus Jakarta Sans", 1, 14)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("USERNAME");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 220, 120, 30));
+
+        jLabel5.setFont(new java.awt.Font("Playfair Display", 1, 24)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Login As Org Admin");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 150, -1, -1));
 
         usernameInput.setFont(new java.awt.Font("Plus Jakarta Sans", 0, 14)); // NOI18N
         usernameInput.setBorder(null);
@@ -125,27 +127,14 @@ public class loginAsOso extends javax.swing.JFrame {
         });
         jPanel1.add(usernameInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(295, 254, 420, 40));
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/input.png"))); // NOI18N
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 250, -1, -1));
+        jLabel4.setFont(new java.awt.Font("Plus Jakarta Sans", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("PASSWORD");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 310, 100, 30));
 
         passwordInput.setFont(new java.awt.Font("Plus Jakarta Sans", 0, 14)); // NOI18N
         passwordInput.setBorder(null);
         jPanel1.add(passwordInput, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 340, 420, 40));
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/input.png"))); // NOI18N
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 340, -1, -1));
-
-        login.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/loginUserbtn.png"))); // NOI18N
-        login.setBorder(null);
-        login.setBorderPainted(false);
-        login.setContentAreaFilled(false);
-        login.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/loginHover.png"))); // NOI18N
-        login.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                loginActionPerformed(evt);
-            }
-        });
-        jPanel1.add(login, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 430, -1, -1));
 
         backBtn.setFont(new java.awt.Font("Plus Jakarta Sans", 0, 18)); // NOI18N
         backBtn.setForeground(new java.awt.Color(255, 255, 255));
@@ -159,6 +148,24 @@ public class loginAsOso extends javax.swing.JFrame {
             }
         });
         jPanel1.add(backBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 80, 30));
+
+        login.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/loginUserbtn.png"))); // NOI18N
+        login.setBorder(null);
+        login.setBorderPainted(false);
+        login.setContentAreaFilled(false);
+        login.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/loginHover.png"))); // NOI18N
+        login.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                loginActionPerformed(evt);
+            }
+        });
+        jPanel1.add(login, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 430, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/input.png"))); // NOI18N
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 340, -1, -1));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/input.png"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 250, -1, -1));
 
         navbar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/bond/assets/UserImages/navBar_1.png"))); // NOI18N
         jPanel1.add(navbar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, -1));
@@ -187,6 +194,13 @@ public class loginAsOso extends javax.swing.JFrame {
 
     }//GEN-LAST:event_usernameInputActionPerformed
 
+    private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBtnActionPerformed
+        // TODO add your handling code here:
+         new loginChoices().setVisible(true);
+        this.dispose();
+        
+    }//GEN-LAST:event_backBtnActionPerformed
+
     private void loginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginActionPerformed
         // TODO add your handling code here:
 
@@ -209,13 +223,6 @@ public class loginAsOso extends javax.swing.JFrame {
         }*/
     }//GEN-LAST:event_loginActionPerformed
 
-    private void backBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_backBtnActionPerformed
-        // TODO add your handling code here:
-
-                 new loginChoices().setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_backBtnActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -233,20 +240,20 @@ public class loginAsOso extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(loginAsOso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(loginAsAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(loginAsOso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(loginAsAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(loginAsOso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(loginAsAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(loginAsOso.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(loginAsAdmin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new loginAsOso().setVisible(true);
+                new loginAsAdmin().setVisible(true);
             }
         });
     }
@@ -258,6 +265,7 @@ public class loginAsOso extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton login;
